4ff11f521247481e8b0ccde46dcd593f930cfa21
opmallen <80222254+opmallen@users.noreply.github.com>
2023-09-19 17:40:28 +0700

    House.js
    Transplanted HTML from prev project

--- src/pages/house.jsx
+++ src/pages/house.jsx
@@ -1,3 +1,211 @@
 function house() {
-  return <span>This is the house page.</span>;
+  return (
+    <div>
+      <div class="p-2 border-bottom border-secondary-subtle">
+        <div class="container">
+          <div class="row row-cols-2 align-items-center">
+            <div class="col">
+              <img
+                src="images/logo-airbnb.png"
+                width="100px"
+                alt="Airbnb.com"
+              />
+            </div>
+            <div class="col text-end">
+              <a
+                href="www.google.com"
+                type="button"
+                class="btn btn-outline-secondary"
+              >
+                <img
+                  src="https://randomuser.me/api/portraits/men/11.jpg"
+                  width="20px"
+                />
+                Oisin
+              </a>
+              <a
+                href="www.google.com"
+                type="button"
+                class="btn btn-outline-secondary"
+              >
+                Logout
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="border-bottom border-secondary-subtle">
+        <div class="container g-0 p-0">
+          <div class="row row-cols-2">
+            <div class="col-6">
+              <img
+                src="https://res.cloudinary.com/dsko6ntfj/image/upload/v1640295026/portal/web%20development%20beginners/05%20Project%20Airbnb/house%2001/house_01_01.png"
+                width="100%"
+              />
+            </div>
+            <div class="col-6">
+              <div class="row">
+                <div class="container">
+                  <div class="row row-cols-3 gx-1">
+                    <div class="col m-0 p-1">
+                      <img
+                        src="https://res.cloudinary.com/dsko6ntfj/image/upload/v1640295026/portal/web%20development%20beginners/05%20Project%20Airbnb/house%2001/house_01_01.png"
+                        width="100%"
+                      />
+                    </div>
+                    <div class="col m-0 g-0 p-1">
+                      <img
+                        src="https://res.cloudinary.com/dsko6ntfj/image/upload/v1640295026/portal/web%20development%20beginners/05%20Project%20Airbnb/house%2001/house_01_02.png"
+                        width="100%"
+                      />
+                    </div>
+                    <div class="col m-0 g-0 p-1">
+                      <img
+                        src="https://res.cloudinary.com/dsko6ntfj/image/upload/v1640295026/portal/web%20development%20beginners/05%20Project%20Airbnb/house%2001/house_01_03.png"
+                        width="100%"
+                      />
+                    </div>
+                    <div class="col m-0 g-0 p-1">
+                      <img
+                        src="https://res.cloudinary.com/dsko6ntfj/image/upload/v1640295026/portal/web%20development%20beginners/05%20Project%20Airbnb/house%2001/house_01_04.png"
+                        width="100%"
+                      />
+                    </div>
+                    <div class="col m-0 g-0 p-1">
+                      <img
+                        src="https://res.cloudinary.com/dsko6ntfj/image/upload/v1640295026/portal/web%20development%20beginners/05%20Project%20Airbnb/house%2001/house_01_05.png"
+                        width="100%"
+                      />
+                    </div>
+                    <div class="col m-0 g-0 p-1">
+                      <img
+                        src="https://res.cloudinary.com/dsko6ntfj/image/upload/v1640295026/portal/web%20development%20beginners/05%20Project%20Airbnb/house%2001/house_01_06.png"
+                        width="100%"
+                      />
+                    </div>
+                    <div class="col m-0 g-0 p-1">
+                      <img
+                        src="https://res.cloudinary.com/dsko6ntfj/image/upload/v1640295026/portal/web%20development%20beginners/05%20Project%20Airbnb/house%2001/house_01_07.png"
+                        width="100%"
+                      />
+                    </div>
+                    <div class="col m-0 g-0 p-1">
+                      <img
+                        src="https://res.cloudinary.com/dsko6ntfj/image/upload/v1640295026/portal/web%20development%20beginners/05%20Project%20Airbnb/house%2001/house_01_08.png"
+                        width="100%"
+                      />
+                    </div>
+                    <div class="col m-0 g-0 p-1">
+                      <img
+                        src="https://res.cloudinary.com/dsko6ntfj/image/upload/v1640295026/portal/web%20development%20beginners/05%20Project%20Airbnb/house%2001/house_01_09.png"
+                        width="100%"
+                      />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="container pt-5">
+        <div class="row row-cols-2 gx-5 justify-content-between">
+          <div class="col-7">
+            <div>
+              <h1>Luxury Villa in Chaweng</h1>
+              <span>
+                <small>
+                  <i class="fa-solid fa-location-dot"></i> Koh Samui • 4 Rooms
+                </small>
+              </span>
+              <div class="container">
+                <div class="row row-cols-2">
+                  <div class="col-1">
+                    <img
+                      src="https://randomuser.me/api/portraits/men/11.jpg"
+                      width="40px"
+                    />
+                  </div>
+                  <div class="col">
+                    <span class="d-block">
+                      <small>Hosted by</small>
+                    </span>
+                    <span>Oisin Allen</span>
+                  </div>
+                </div>
+              </div>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                Risus feugiat in ante metus dictum at tempor commodo. Purus in
+                massa tempor nec feugiat nisl. Non curabitur gravida arcu ac
+                tortor. Arcu non sodales neque sodales ut etiam sit amet nisl.
+                Diam maecenas sed enim ut sem viverra aliquet eget. Amet nisl
+                purus in mollis. Viverra vitae congue eu consequat ac felis
+                donec et odio.
+              </p>
+            </div>
+            <div>
+              <h2>0 Reviews</h2>
+              <span class="d-block">Leave a review</span>
+              <form>
+                <textarea rows="7" class="d-block"></textarea>
+                <span class="d-block">
+                  <i class="fa-solid fa-thumbs-up"></i>{" "}
+                  <input type="radio" name="review" />
+                  <i class="fa-solid fa-thumbs-down"></i>{" "}
+                  <input type="radio" name="review" />
+                </span>
+                <button type="button" class="btn btn-success">
+                  Submit
+                </button>
+              </form>
+            </div>
+          </div>
+          <div class="col-5">
+            {/* <!-- request box --> */}
+            <div class="card" style="width: 18rem;">
+              <div class="card-body">
+                <h2 class="card-title">$$$$/night</h2>
+                <span>
+                  <small>X Reviews</small>
+                </span>
+                <textarea
+                  rows="7"
+                  placeholder="Send the host a message..."
+                  class="d-block"
+                ></textarea>
+                <a href="#" class="btn btn-success">
+                  Request Booking
+                </a>
+              </div>
+            </div>
+          </div>
+          <div class="col-7">
+            <div>
+              <h2>0 Reviews</h2>
+              <span class="d-block">Leave a review</span>
+              <form>
+                <textarea rows="7" class="d-block"></textarea>
+                <span class="d-block">
+                  <label>Thumbs-Up</label> <input type="radio" name="review" />
+                  <label>Thumbs-Down</label>{" "}
+                  <input type="radio" name="review" />
+                </span>
+                <button type="button" class="btn btn-success">
+                  Submit
+                </button>
+              </form>
+            </div>
+            <div>
+              existing reviews
+              <span>a list of previous reviews</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
+
+export default House;
